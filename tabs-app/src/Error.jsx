4b3b8b1529ab5b0eb32cfb91509d28eca451f20d
DForant <dforant@gmail.com>
2025-04-28 20:01:09 -0400

    update package.json files

--- tabs-app/src/Error.jsx
+++ tabs-app/src/Error.jsx
@@ -1,7 +1,7 @@
 const Error = () => {
   return (
     <div className='alert alert-danger'>
-        <h2 className='title'>An error occuered while Fetching data.</h2>
+        <h2 className='title'>An error occurred while Fetching data.</h2>
     </div>
   )
 }
